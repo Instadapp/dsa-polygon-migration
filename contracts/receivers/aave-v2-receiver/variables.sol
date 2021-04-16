577b2d5c41f6eb51ab6e96889a4e5f053a7a3c41
Thrilok Kumar <thrilok2000@gmail.com>
2021-04-16 22:02:00 +0530

    restructured aave receiver variable contract

--- contracts/receivers/aave-v2-receiver/variables.sol
+++ contracts/receivers/aave-v2-receiver/variables.sol
@@ -9,35 +9,63 @@ import {
 } from "./interfaces.sol";
 
 contract Variables {
+    // Constant Address //
 
-    // This will be used to have debt/collateral ratio always 20% less than liquidation
-    // TODO: Is this number correct for it?
-    uint public safeRatioGap = 800000000000000000; // 20%? 2e17
-
-    // TODO: Replace this
-    TokenMappingInterface tokenMapping = TokenMappingInterface(address(0xa31442F2607947a88807b2bcD5D4951eEdd4A885)); // TODO: FAKE ADDR, CHANGE THIS
-
+    /**
+    * @dev token Mapping contract Provider
+    */
+    TokenMappingInterface constant public tokenMapping = TokenMappingInterface(address(0xa31442F2607947a88807b2bcD5D4951eEdd4A885)); // TODO @thrilok209: FAKE ADDR, CHANGE THIS
+    /**
+     * @dev Aave Provider
+     */
     AaveLendingPoolProviderInterface constant internal aaveProvider = AaveLendingPoolProviderInterface(0xd05e3E715d945B59290df0ae8eF85c1BdB684744);
-
+    
     /**
      * @dev Aave Data Provider
      */
     AaveDataProviderInterface constant internal aaveData = AaveDataProviderInterface(0x7551b5D2763519d4e37e8B81929D336De671d46d);
+    
 
-
-    // dsa => position
-    mapping(uint => bytes) public positions;
-
-    // InstaIndex Address.
+    /**
+     * @dev InstaIndex Polygon contract
+     */
     IndexInterface public constant instaIndex = IndexInterface(0xA9B99766E6C676Cf1975c0D3166F96C0848fF5ad);
 
-    // TODO: Set by construtor?
+     /**
+     * @dev InstaList Polygon contract
+     */
+    InstaListInterface public constant instaList = InstaListInterface(0x839c2D3aDe63DF5b0b8F3E57D5e145057Ab41556);
+
+
+    /**
+     * @dev Matic StateReceiver contract
+     */
+    address public constant maticReceiver = 0x0000000000000000000000000000000000001001;
+
+
+    // Storage variables //
+
+    /**
+     * @dev This will be used to have debt/collateral ratio always 20% less than liquidation
+    */
+    // TODO @KaymasJain: Is this number correct for it?
+    uint public safeRatioGap = 800000000000000000; // 20%? 2e17
+
+    // mapping stateId => user position
+    mapping(uint => bytes) public positions;
+
+    /**
+    * @dev Mapping of supported token
+    */
     mapping(address => bool) public isSupportedToken;
+
+    /**
+    * @dev Array of supported token
+    */
     address[] public supportedTokens; // don't add maticAddr. Only add wmaticAddr?
 
-    // Address which will receive L1 data and post it on L2
-    address public maticReceiver = 0x0000000000000000000000000000000000001001;
-
-    InstaListInterface public instaList = InstaListInterface(0x839c2D3aDe63DF5b0b8F3E57D5e145057Ab41556);
-
+    /**
+    * @dev last stateId from the onStateReceive
+    */
+    uint256 internal lastStateId;
 }
